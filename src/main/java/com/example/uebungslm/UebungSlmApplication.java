8dbbac727bf8d1b39b03939ed610ada3f76927c8
VanessaMaria02 <wi21b004@technikum-wien.at>
2022-06-20 18:10:10 +0200

    Initial commit

---
+++ src/main/java/com/example/uebungslm/UebungSlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.uebungslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UebungSlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uebungslm/UebungSlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebungslm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungSlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
